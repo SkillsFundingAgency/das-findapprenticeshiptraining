a1a366d800f8ae04bf40962368c3021bba6ebfae
James King <hello@jamesmichaelking.com>
2022-06-21 12:22:50 +0100

    Styles and JS update for Employer feedback graphs

--- src/SFA.DAS.FAT.Web/wwwroot/js/site.js
+++ src/SFA.DAS.FAT.Web/wwwroot/js/site.js
@@ -223,6 +223,7 @@ function nodeListForEach(nodes, callback) {
 function FeedbackGraph(table) {
     this.table = table
     this.target = this.table.dataset.target
+    this.hideLegend = this.table.dataset.hideLegend === "true"
     this.label = this.table.dataset.label || "people"
     this.rows = this.table.querySelectorAll("tbody tr")
 }
@@ -241,6 +242,9 @@ FeedbackGraph.prototype.init = function() {
     var graphList = document.createElement("ul")
         graphList.className = "app-graph__list"
 
+    var caption = this.table.querySelector("caption")
+    var categoryHtml
+
     nodeListForEach(this.rows, function (row) {
         if (rowCount === 0) {
             legendlistHtml = that.legendHtml(row)
@@ -248,7 +252,18 @@ FeedbackGraph.prototype.init = function() {
         graphList.appendChild(that.graphRow(row))
         rowCount++
     });
-    graphHtml.appendChild(legendlistHtml)
+
+    if (!this.hideLegend) {
+        graphHtml.appendChild(legendlistHtml)
+    }
+    
+    if (caption) {
+        categoryHtml = document.createElement("h2")
+        categoryHtml.className = "govuk-caption-l app-graph__category"
+        categoryHtml.textContent = caption.textContent
+        graphHtml.appendChild(categoryHtml)
+    }
+
     graphHtml.appendChild(graphList)
     document.getElementById(this.target).appendChild(graphHtml)
 }
@@ -258,6 +273,10 @@ FeedbackGraph.prototype.legendHtml = function(row) {
         legendList.className = "app-graph-key"
     var dataCells = row.querySelectorAll("td")
     var cellCount = 0
+
+    if (this.hideLegend) {
+        return ''
+    }
 
     nodeListForEach(dataCells, function (dataCell) {
         if (isNaN(dataCell.textContent)) {
@@ -303,7 +322,9 @@ FeedbackGraph.prototype.graphRow = function(row) {
     var heading = document.createElement('h3')
         heading.className = "app-graph__label"
         heading.textContent = questionText
-        heading.appendChild(caption)
+        if (totalAsked > 0) {
+            heading.appendChild(caption)
+        }
 
         graphRowHtml.appendChild(heading)
         graphRowHtml.appendChild(barsHtml)
