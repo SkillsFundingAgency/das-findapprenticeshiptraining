1e561bc9ad9585e7c86f2a48ab1fd695e02a9e0e
MarkFCain <mark.cain@digital.education.gov.uk>
2024-02-23 09:35:04 +0000

    CSP-1274 remove unneeded files

--- src/SFA.DAS.FAT.Domain/Validation/Shortlist/ShortlistForUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SFA.DAS.FAT.Domain.Shortlist
-{
-    public class ShortlistForUser
-    {
-        public IEnumerable<ShortlistItem> Shortlist { get; set; }
-    }
-}
