eb6973e55b288a7e47f6b01667d3330e1fc3e0e8
Deven Shah <devenshah1974@gmail.com>
2025-01-17 14:09:37 +0000

    CSP-1826 Implements session and application level caching service (#202)
    * CSP-1826 Added session service
    
    * CSP-1826 Added disctributed service for application level caching
    
    * CSP-1826 Fixed minor compile time warnings
    
    * CSP-1826 Added Contains method to SessionService
    
    * CSP-1826 Removed redundant test

---
+++ src/SFA.DAS.FAT.Domain/Interfaces/IDistributedCacheService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.FAT.Domain.Interfaces;
+
+public interface IDistributedCacheService
+{
+    Task<T> GetOrSetAsync<T>(string cacheKey, Func<Task<T>> getData, TimeSpan cacheDuration);
+    Task RemoveAsync(string cacheKey);
+}
---
+++ src/SFA.DAS.FAT.Web/AppStart/AddSessionExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.FAT.Domain.Configuration;
+using SFA.DAS.FAT.Domain.Interfaces;
+using SFA.DAS.FAT.Web.Services;
+
+namespace SFA.DAS.FAT.Web.AppStart;
+
+[ExcludeFromCodeCoverage]
+public static class AddSessionExtension
+{
+    public static IServiceCollection AddSession(this IServiceCollection services, IConfigurationRoot configuration)
+    {
+        services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(30);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.Cookie.IsEssential = true;
+        });
+        services.AddHttpContextAccessor();
+        services.AddScoped<ISessionService, SessionService>();
+        services.AddSingleton<IDistributedCacheService, IDistributedCacheService>();
+        AddDistributedCache(services, configuration);
+        return services;
+    }
+
+    private static void AddDistributedCache(IServiceCollection services, IConfigurationRoot configuration)
+    {
+        var localEnvs = new[] { "LOCAL", "DEV" };
+        if (localEnvs.Contains(configuration["EnvironmentName"]))
+        {
+            services.AddDistributedMemoryCache();
+        }
+        else
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                var fatWebConfig = configuration.GetSection(nameof(FindApprenticeshipTrainingWeb)).Get<FindApprenticeshipTrainingWeb>();
+                options.Configuration = fatWebConfig.RedisConnectionString;
+            });
+        }
+    }
+}
