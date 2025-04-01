e379fd70ac9bcc6bd3b759b66f42a652cca81f32
Deven Shah <devenshah1974@gmail.com>
2025-04-01 18:15:02 +0100

    CSP-1726 Updates shortlist page with new content
    CSP-1726 Updates shortlist page with new content
    
    CSP-1726 Minor UI fixes

---
+++ src/SFA.DAS.FAT.Web.UnitTests/Models/ShortlistsViewModelTests/WhenCreatingShortlistsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Web.Models;
+
+namespace SFA.DAS.FAT.Web.UnitTests.Models.ShortlistsViewModelTests;
+
+public class WhenCreatingShortlistsViewModel
+{
+    [TestCase("provider name", true)]
+    [TestCase("", false)]
+    [TestCase(null, false)]
+    public void ThenShowRemovedShortlistBannerIsAsExpected(string removedProviderName, bool expected)
+    {
+        ShortlistsViewModel sut = new()
+        {
+            RemovedProviderName = removedProviderName
+        };
+
+        sut.ShowRemovedShortlistBanner.Should().Be(expected);
+    }
+}
