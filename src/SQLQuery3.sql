e1ef5faec766a409d1229b5850cac9a6c3bc679f
Deven Shah <devenshah1974@gmail.com>
2025-04-04 15:17:46 +0100

    CSP-1995 Fixed TP results now showing multiple locations

--- src/SQLQuery3.sql
+++
@@ -1,9 +0,0 @@
-﻿select pc.LarsCode, s.Title, p.Ukprn, p.LegalName, count(*) 
-from ProviderCourseLocation pcl
-inner join ProviderCourse pc on pc.Id = pcl.ProviderCourseId
-inner join Standard s on s.LarsCode = pc.LarsCode
-inner join Provider p on p.Id = pc.ProviderId
-inner join ProviderLocation pl on pl.Id = pcl.ProviderLocationId and pl.LocationType = 0 
-where pcl.HasBlockReleaseDeliveryOption = 1
-group by pc.LarsCode, s.Title, p.Ukprn, p.LegalName
-having count(*) > 2
