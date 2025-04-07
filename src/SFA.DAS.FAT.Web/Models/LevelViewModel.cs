2345d49878637f02b6387ec1018aa45f0126ee79
Deven Shah <devenshah1974@gmail.com>
2025-04-07 17:33:56 +0100

    CSP-1922 fixed level description in selected filters section in courses result page.

--- src/SFA.DAS.FAT.Web/Models/LevelViewModel.cs
+++ src/SFA.DAS.FAT.Web/Models/LevelViewModel.cs
@@ -1,26 +1,25 @@
 ﻿using System.Collections.Generic;
 using SFA.DAS.FAT.Domain.Courses;
 
-namespace SFA.DAS.FAT.Web.Models
+namespace SFA.DAS.FAT.Web.Models;
+
+public class LevelViewModel
 {
-    public class LevelViewModel
+    public LevelViewModel()
     {
-        public LevelViewModel()
-        {
-
-        }
-
-        public LevelViewModel(Level level, ICollection<int> selectedCodes)
-        {
-            Selected = selectedCodes?.Contains(level.Code) ?? false;
-            Code = level.Code;
-            Name = level.Name;
-        }
-
-        public bool Selected { get; }
-        public int Code { get; set; }
-        public string Name { get; set; }
-        public string Title => $"Level {Code} - {Name}";
 
     }
+
+    public LevelViewModel(Level level, ICollection<int> selectedCodes)
+    {
+        Selected = selectedCodes?.Contains(level.Code) ?? false;
+        Code = level.Code;
+        Name = level.Name;
+    }
+
+    public bool Selected { get; }
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public string Title => $"Level {Code} - {Name}";
+
 }
