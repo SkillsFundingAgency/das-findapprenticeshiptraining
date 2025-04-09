5b995cdfb4820594b6deb90d6206164920cb6cd0
MarkFCain <mark.cain@digital.education.gov.uk>
2025-04-09 14:08:32 +0100

    CSP-1686 Training provider details page generic

---
+++ src/SFA.DAS.FAT.Domain.UnitTests/Providers/Api/WhenCreatingGetProviderApiRequest.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Providers.Api.Requests;
+
+namespace SFA.DAS.FAT.Domain.UnitTests.Providers.Api;
+public class WhenCreatingGetProviderApiRequest
+{
+    [Test, AutoData]
+    public void Then_The_Url_Is_Correctly_Built(string baseUrl, int ukprn)
+    {
+        //Arrange Act
+        var actual = new GetProviderApiRequest(baseUrl, ukprn);
+
+        //Assert
+        actual.GetUrl.Should().Be($"{baseUrl}providers/{ukprn}");
+    }
+}
