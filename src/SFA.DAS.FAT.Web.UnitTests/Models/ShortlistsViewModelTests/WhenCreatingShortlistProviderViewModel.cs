9f2b142bf08f658915c6d8a24f99469d0a7a2c49
Deven Shah <devenshah1974@gmail.com>
2025-04-28 11:23:50 +0100

    CSP-1939 Various UI fixes (#244)
    * CSP-1939 Fixes minor content on shortlist details
    
    * CSP-1939 Fixes UI alignment on providers result
    
    * CSP-1939 Fixes reviews inset text on provider details

--- src/SFA.DAS.FAT.Web.UnitTests/Models/ShortlistsViewModelTests/WhenCreatingShortlistProviderViewModel.cs
+++ src/SFA.DAS.FAT.Web.UnitTests/Models/ShortlistsViewModelTests/WhenCreatingShortlistProviderViewModel.cs
@@ -6,32 +6,6 @@ namespace SFA.DAS.FAT.Web.UnitTests.Models.ShortlistsViewModelTests;
 
 public class WhenCreatingShortlistProviderViewModel
 {
-    [TestCase(1, "Block release")]
-    [TestCase(2, "Block release at multiple locations")]
-    public void ThenBlockReleaseTextIsAsExpected(int count, string expected)
-    {
-        ShortlistProviderViewModel sut = new()
-        {
-            HasBlockRelease = true,
-            BlockReleaseDistance = 10,
-            BlockReleaseCount = count
-        };
-        sut.BlockReleaseText.Should().Be(expected);
-    }
-
-    [TestCase(1, "Day release")]
-    [TestCase(2, "Day release at multiple locations")]
-    public void ThenDayReleaseTextIsAsExpected(int count, string expected)
-    {
-        ShortlistProviderViewModel sut = new()
-        {
-            HasDayRelease = true,
-            DayReleaseDistance = 10,
-            DayReleaseCount = count
-        };
-        sut.DayReleaseText.Should().Be(expected);
-    }
-
     [TestCase(true, true, true, 3)]
     [TestCase(true, false, true, 2)]
     [TestCase(true, true, false, 2)]
