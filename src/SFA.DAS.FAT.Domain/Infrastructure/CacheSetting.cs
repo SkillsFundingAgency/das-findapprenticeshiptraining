5b6b8117361779af84a2ea0e0f4cbbf70de34397
Deven Shah <devenshah1974@gmail.com>
2025-04-30 12:51:59 +0100

    CSP-2055 Changed cache keys for levels and routes

--- src/SFA.DAS.FAT.Domain/Infrastructure/CacheSetting.cs
+++ src/SFA.DAS.FAT.Domain/Infrastructure/CacheSetting.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.FAT.Domain.Infrastructure;
 public static class CacheSetting
 {
     public static CacheInfo Providers => new(CacheKeys.RegisteredProviders, TimeSpan.FromHours(1));
-    public static CacheInfo Levels => new(CacheKeys.Levels, TimeSpan.FromHours(24));
-    public static CacheInfo Routes => new(CacheKeys.Routes, TimeSpan.FromHours(24));
+    public static CacheInfo Levels => new("CourseLevels", TimeSpan.FromHours(24));
+    public static CacheInfo Routes => new("CourseRoutes", TimeSpan.FromHours(24));
     public static CacheInfo AcademicYearsLatest => new(CacheKeys.AcademicYearsLatest, TimeSpan.FromHours(24));
 }
