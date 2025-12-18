7cb5e9073418bdfe668029153ead0ef69c4a64cf
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2025-12-18 15:12:50 +0000

    CSP-2337 Refactor GetCourseQueryValidatorTests to use [TestCase]

--- src/SFA.DAS.FAT.Web.UnitTests/Validators/GetCourseQueryValidatorTests.cs
+++ src/SFA.DAS.FAT.Web.UnitTests/Validators/GetCourseQueryValidatorTests.cs
@@ -16,120 +16,35 @@ public class GetCourseQueryValidatorTests
         _validator = new GetCourseQueryValidator();
     }
 
-    [Test]
-    public void TestValidator_LarsCodeIsZero_ReturnsExpectedErrorMessage()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = "0" });
-
-        result.IsValid.Should().BeFalse();
-        result.ShouldHaveValidationErrorFor(c => c.LarsCode)
-            .WithErrorMessage(GetCourseQueryValidator.CourseIdErrorMessage);
-    }
-
-    [Test]
-    public void TestValidator_LarsCodeIsNegative_ReturnsExpectedErrorMessage()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = "-1" });
-
-        result.IsValid.Should().BeFalse();
-        result.ShouldHaveValidationErrorFor(c => c.LarsCode)
-            .WithErrorMessage(GetCourseQueryValidator.CourseIdErrorMessage);
-    }
-
-    [Test]
-    public void TestValidator_LarsCodeIsNull_ReturnsExpectedErrorMessage()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = null });
-
-        result.IsValid.Should().BeFalse();
-        result.ShouldHaveValidationErrorFor(c => c.LarsCode)
-            .WithErrorMessage(GetCourseQueryValidator.CourseIdErrorMessage);
-    }
-
-    [Test]
-    public void TestValidator_LarsCodeIsEmpty_ReturnsExpectedErrorMessage()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = string.Empty });
-
-        result.IsValid.Should().BeFalse();
-        result.ShouldHaveValidationErrorFor(c => c.LarsCode)
-            .WithErrorMessage(GetCourseQueryValidator.CourseIdErrorMessage);
-    }
-
-    [Test]
-    public void TestValidator_LarsCodeIsWhitespace_ReturnsExpectedErrorMessage()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = "   " });
-
-        result.IsValid.Should().BeFalse();
-        result.ShouldHaveValidationErrorFor(c => c.LarsCode)
-            .WithErrorMessage(GetCourseQueryValidator.CourseIdErrorMessage);
-    }
-
-    [Test]
-    public void TestValidator_LarsCodeIsNotNumeric_ReturnsExpectedErrorMessage()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = "abc" });
-
-        result.IsValid.Should().BeFalse();
-        result.ShouldHaveValidationErrorFor(c => c.LarsCode)
-            .WithErrorMessage(GetCourseQueryValidator.CourseIdErrorMessage);
-    }
-
-    [Test]
-    public void TestValidator_LarsCodeIsAlphanumeric_ReturnsExpectedErrorMessage()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = "123abc" });
-
-        result.IsValid.Should().BeFalse();
-        result.ShouldHaveValidationErrorFor(c => c.LarsCode)
-            .WithErrorMessage(GetCourseQueryValidator.CourseIdErrorMessage);
-    }
-
-    [Test]
-    public void TestValidator_LarsCodeIsValidPositiveInteger_ReturnsValid()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = "1" });
-
-        result.IsValid.Should().BeTrue();
-        result.ShouldNotHaveValidationErrorFor(c => c.LarsCode);
-    }
-
-    [Test]
-    public void TestValidator_LarsCodeIsValidLargeNumber_ReturnsValid()
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = "12345" });
-
-        result.IsValid.Should().BeTrue();
-        result.ShouldNotHaveValidationErrorFor(c => c.LarsCode);
-    }
-
-    [TestCase("123")]
-    [TestCase("999")]
-    [TestCase("1")]
-    [TestCase("2147483647")] // int.MaxValue
-    public void TestValidator_LarsCodeIsValidNumericString_ReturnsValid(string larsCode)
-    {
-        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = larsCode });
-
-        result.IsValid.Should().BeTrue();
-        result.ShouldNotHaveValidationErrorFor(c => c.LarsCode);
-    }
-
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("  ")]
     [TestCase("0")]
     [TestCase("-1")]
     [TestCase("-100")]
-    [TestCase("")]
-    [TestCase("  ")]
     [TestCase("abc")]
+    [TestCase("123abc")]
     [TestCase("12.5")]
     [TestCase("2147483648")] // int.MaxValue + 1
-    public void TestValidator_LarsCodeIsInvalid_ReturnsExpectedErrorMessage(string larsCode)
+    public void LarsCode_InvalidValues_ShouldHaveValidationError(string larsCode)
     {
         var result = _validator.TestValidate(new GetCourseQuery { LarsCode = larsCode });
 
         result.IsValid.Should().BeFalse();
         result.ShouldHaveValidationErrorFor(c => c.LarsCode)
             .WithErrorMessage(GetCourseQueryValidator.CourseIdErrorMessage);
+    }
+
+    [TestCase("1")]
+    [TestCase("123")]
+    [TestCase("999")]
+    [TestCase("12345")]
+    [TestCase("2147483647")] // int.MaxValue
+    public void LarsCode_ValidNumericStrings_ShouldBeValid(string larsCode)
+    {
+        var result = _validator.TestValidate(new GetCourseQuery { LarsCode = larsCode });
+
+        result.IsValid.Should().BeTrue();
+        result.ShouldNotHaveValidationErrorFor(c => c.LarsCode);
     }
 }
