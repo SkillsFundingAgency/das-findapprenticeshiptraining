e2a8918c215fe8da834468d82bf609ead2d67232
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-02-11 17:35:50 +0000

    CSP-2419 Improving code coverage

--- src/SFA.DAS.FAT.Web.UnitTests/Models/CoursesViewModelFilterTests/WhenFilteringCourses.cs
+++ src/SFA.DAS.FAT.Web.UnitTests/Models/CoursesViewModelFilterTests/WhenFilteringCourses.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.FAT.Domain.Configuration;
@@ -9,6 +10,7 @@ using SFA.DAS.FAT.Web.Models;
 using SFA.DAS.FAT.Web.Models.Filters.FilterComponents;
 using SFA.DAS.FAT.Web.Models.Filters.Helpers;
 using SFA.DAS.FAT.Web.Services;
+using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.FAT.Web.UnitTests.Models.CoursesViewModelFilterTests;
 
@@ -300,5 +302,199 @@ public sealed class WhenFilteringCourses
             var selectedItem = ((DropdownFilterSectionViewModel)distanceFilterSection).Items.First(a => a.IsSelected);
             Assert.That(selectedItem.Value, Is.EqualTo(DistanceService.TEN_MILES.ToString()));
         });
+    }
+
+    [Test]
+    public void GetLevelName_Returns_Formatted_Name_When_Level_Exists()
+    {
+        var result = _coursesViewModel.GetLevelName(3);
+        Assert.That(result, Is.EqualTo("3 - equal to Level 3"));
+    }
+
+    [Test]
+    public void GetLevelName_Returns_Empty_When_Level_Does_Not_Exist()
+    {
+        var result = _coursesViewModel.GetLevelName(9);
+        Assert.That(result, Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void OrderBy_Is_Score_When_Keyword_Set_Else_Title()
+    {
+        Assert.That(_coursesViewModel.OrderBy, Is.EqualTo(OrderBy.Score));
+
+        var vm = new CoursesViewModel(_findApprenticeshipTrainingWebConfiguration, _urlHelperMock.Object)
+        {
+            Keyword = string.Empty
+        };
+
+        Assert.That(vm.OrderBy, Is.EqualTo(OrderBy.Title));
+    }
+
+    [Test]
+    public void SortedDisplayMessage_Matches_OrderBy()
+    {
+        Assert.That(_coursesViewModel.SortedDisplayMessage, Is.EqualTo(CoursesViewModel.BEST_MATCH_TO_COURSE));
+
+        var vm = new CoursesViewModel(_findApprenticeshipTrainingWebConfiguration, _urlHelperMock.Object)
+        {
+            Keyword = string.Empty
+        };
+
+        Assert.That(vm.SortedDisplayMessage, Is.EqualTo(CoursesViewModel.NAME_OF_COURSE));
+    }
+
+    [Test, MoqAutoData]
+    public void GetProvidersLinkDisplayMessage_No_Providers_Asks_Training_Provider(StandardViewModel standard)
+    {
+        standard.LarsCode = "1";
+        standard.ProvidersCount = 0;
+        standard.StandardUId = "ST0001";
+        standard.IfateReferenceNumber = "ST0001";
+        standard.Title = "Test Standard";
+        standard.OverviewOfRole = "Test Overview";
+        standard.Keywords = "Test Keywords";
+        standard.Route = "Test Route";
+
+        var message = _coursesViewModel.GetProvidersLinkDisplayMessage(standard);
+        Assert.That(message, Is.EqualTo(CoursesViewModel.ASK_TRAINING_PROVIDER));
+    }
+
+    [Test, MoqAutoData]
+    public void GetProvidersLinkDisplayMessage_National_Search_Without_Location(StandardViewModel standard)
+    {
+        var vm = new CoursesViewModel(_findApprenticeshipTrainingWebConfiguration, _urlHelperMock.Object)
+        {
+            Location = string.Empty,
+            Distance = DistanceService.ACROSS_ENGLAND_FILTER_VALUE
+        };
+
+        standard.LarsCode = "1";
+        standard.ProvidersCount = 2;
+
+        var message = vm.GetProvidersLinkDisplayMessage(standard);
+        Assert.That(message, Is.EqualTo("View 2 training providers for this course"));
+    }
+
+    [Test, MoqAutoData]
+    public void GetProvidersLinkDisplayMessage_Local_Search_With_Distance(StandardViewModel standard)
+    {
+        var vm = new CoursesViewModel(_findApprenticeshipTrainingWebConfiguration, _urlHelperMock.Object)
+        {
+            Location = "SW1A 1AA",
+            Distance = "30"
+        };
+
+        standard.LarsCode = "1";
+        standard.ProvidersCount = 1;
+
+        var message = vm.GetProvidersLinkDisplayMessage(standard);
+        Assert.That(message, Is.EqualTo("View 1 training provider within 30 miles"));
+    }
+
+    [Test, MoqAutoData]
+    public void GetProvidersLink_When_Providers_Available_Uses_RouteUrl(StandardViewModel standard)
+    {
+        _urlHelperMock
+            .Setup(x => x.RouteUrl(It.IsAny<UrlRouteContext>()))
+            .Returns("/courses/1/providers?location=M60%207RA&distance=20");
+
+        standard.LarsCode = "1";
+        standard.ProvidersCount = 3;
+
+        var url = _coursesViewModel.GetProvidersLink(standard);
+        Assert.That(url, Is.EqualTo("/courses/1/providers?location=M60%207RA&distance=20"));
+    }
+
+    [Test, MoqAutoData]
+    public void GetProvidersLink_No_Providers_Returns_Help_Url_With_Location_When_Present(StandardViewModel standard)
+    {
+        var config = new FindApprenticeshipTrainingWeb
+        {
+            RequestApprenticeshipTrainingUrl = "https://localhost",
+            EmployerAccountsUrl = "https://accounts"
+        };
+        var vm = new CoursesViewModel(config, _urlHelperMock.Object)
+        {
+            Location = "M60 7RA"
+        };
+
+        standard.LarsCode = "123";
+        standard.ProvidersCount = 0;
+
+        var url = vm.GetProvidersLink(standard);
+        Assert.That(url, Does.StartWith("https://accounts/service/?redirectUri="));
+        Assert.That(Uri.UnescapeDataString(url), Does.Contain("standardId=123"));
+        Assert.That(Uri.UnescapeDataString(url), Does.Contain("location=M60 7RA"));
+    }
+
+    [Test]
+    public void TotalMessage_Uses_Total_When_No_Filters_Else_TotalFiltered_With_Pluralization()
+    {
+        var vm = new CoursesViewModel(_findApprenticeshipTrainingWebConfiguration, _urlHelperMock.Object)
+        {
+            Total = 1,
+            TotalFiltered = 2,
+            Keyword = string.Empty,
+            SelectedRoutes = [],
+            SelectedLevels = []
+        };
+        Assert.That(vm.TotalMessage, Is.EqualTo("1 result"));
+
+        vm.Keyword = "x";
+        vm.Total = 2;
+        vm.TotalFiltered = 3;
+        Assert.That(vm.TotalMessage, Is.EqualTo("3 results"));
+    }
+
+    [Test]
+    public void CoursesSubHeader_Behavior_With_And_Without_Location()
+    {
+        var vm = new CoursesViewModel(_findApprenticeshipTrainingWebConfiguration, _urlHelperMock.Object)
+        {
+            Total = 5,
+            Location = "M60 7RA",
+            Distance = "20"
+        };
+        Assert.That(vm.CoursesSubHeader, Is.EqualTo("Select the course name to view details about it, or select view training providers to see the training providers who run that course in the apprentice's work location."));
+
+        vm.Location = string.Empty;
+        vm.Distance = DistanceService.ACROSS_ENGLAND_FILTER_VALUE;
+        Assert.That(vm.CoursesSubHeader, Is.EqualTo("Select the course name to view details about it, or select view training providers to see the training providers who run that course."));
+
+        vm.Total = 0;
+        Assert.That(vm.CoursesSubHeader, Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void GenerateStandardRouteValues_Includes_Location_And_Distance_When_Present()
+    {
+        var values = _coursesViewModel.GenerateStandardRouteValues("55");
+        Assert.That(values["id"], Is.EqualTo("55"));
+        Assert.That(values.ContainsKey("location"), Is.True);
+        Assert.That(values.ContainsKey("distance"), Is.True);
+        Assert.That(values["location"], Is.EqualTo("M60 7RA"));
+        Assert.That(values["distance"], Is.EqualTo("20"));
+    }
+
+    [Test]
+    public void ToQueryString_Returns_Selected_Filters_As_KeyValuePairs()
+    {
+        var qs = _coursesViewModel.ToQueryString();
+
+        Assert.That(qs.Any(kv => kv.Item1 == nameof(CoursesViewModel.Keyword) && kv.Item2 == "Construction"), Is.True);
+
+        Assert.That(qs.Any(kv => kv.Item1 == nameof(CoursesViewModel.Location) && kv.Item2 == "M60 7RA"), Is.True);
+        Assert.That(qs.Any(kv => kv.Item1 == nameof(CoursesViewModel.Distance) && kv.Item2 == "20"), Is.True);
+
+        Assert.That(qs.Count(kv => kv.Item1 == nameof(FilterService.FilterType.Levels)), Is.EqualTo(2));
+        Assert.That(qs.Any(kv => kv.Item1 == nameof(FilterService.FilterType.Levels) && kv.Item2 == "3"), Is.True);
+        Assert.That(qs.Any(kv => kv.Item1 == nameof(FilterService.FilterType.Levels) && kv.Item2 == "4"), Is.True);
+
+        Assert.That(qs.Count(kv => kv.Item1 == nameof(FilterService.FilterType.Categories)), Is.EqualTo(1));
+        Assert.That(qs.Any(kv => kv.Item1 == nameof(FilterService.FilterType.Categories) && kv.Item2 == "Construction"), Is.True);
+
+        Assert.That(qs.Count(kv => kv.Item1 == nameof(FilterService.FilterType.ApprenticeshipTypes)), Is.EqualTo(1));
+        Assert.That(qs.Any(kv => kv.Item1 == nameof(FilterService.FilterType.ApprenticeshipTypes) && kv.Item2 == ApprenticeshipType.FoundationApprenticeship.GetDescription()), Is.True);
     }
 }
