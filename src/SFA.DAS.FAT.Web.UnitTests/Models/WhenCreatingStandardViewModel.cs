584ba3da0b378739a4fbfadb5f7def5fdb0eab9e
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-03-23 17:16:30 +0000

    CSP-2420 Added unit tests for StandardViewModel mapping and tags

---
+++ src/SFA.DAS.FAT.Web.UnitTests/Models/WhenCreatingStandardViewModel.cs
@@ -0,0 +1,50 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Courses;
+using SFA.DAS.FAT.Domain.Courses.Api.Responses;
+using SFA.DAS.FAT.Web.Models;
+
+namespace SFA.DAS.FAT.Web.UnitTests.Models;
+
+public class WhenCreatingStandardViewModel
+{
+    [TestCase(ApprenticeshipType.Apprenticeship, "app-apprenticeship-type-tag--apprenticeship")]
+    [TestCase(ApprenticeshipType.FoundationApprenticeship, "app-apprenticeship-type-tag--foundationApprenticeship")]
+    [TestCase(ApprenticeshipType.ApprenticeshipUnit, "app-apprenticeship-type-tag--apprenticeshipUnit")]
+    public void ApprenticeshipTypeTagClass_ForKnownType_ReturnsExpectedCssClass(
+        ApprenticeshipType apprenticeshipType,
+        string expectedCssClass)
+    {
+        var sut = new StandardViewModel
+        {
+            StandardUId = "ST0001",
+            IfateReferenceNumber = "IFATE-0001",
+            Title = "Title",
+            OverviewOfRole = "Overview",
+            Keywords = "Keywords",
+            Route = "Route",
+            ApprenticeshipType = apprenticeshipType
+        };
+
+        sut.ApprenticeshipTypeTagClass.Should().Be(expectedCssClass);
+    }
+
+    [Test, AutoData]
+    public void ApprenticeshipTypeTagClass_ForUnknownType_ReturnsEmptyString(StandardViewModel model)
+    {
+        model.ApprenticeshipType = (ApprenticeshipType)999;
+
+
+        model.ApprenticeshipTypeTagClass.Should().Be(string.Empty);
+    }
+
+    [Test, AutoData]
+    public void ImplicitOperator_FromStandardModel_MapsAllFields(StandardModel source)
+    {
+        StandardViewModel sut = source;
+
+        sut.Should().NotBeNull();
+        sut.Should().BeEquivalentTo(source);
+    }
+}
