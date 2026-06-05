2849ed28dfc6a1dd79da3d9839d71bf0bc480c97
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-06-05 12:39:41 +0100

    CSP-2388 Removed wrapper properties and update filter names

--- src/SFA.DAS.FAT.Web/Models/CoursesViewModel.cs
+++ src/SFA.DAS.FAT.Web/Models/CoursesViewModel.cs
@@ -56,21 +56,6 @@ public class CoursesViewModel : PageLinksViewModelBase
     public List<string> SelectedRoutes { get; set; } = [];
     public List<LearningType> SelectedTrainingTypes { get; set; } = [];
     public List<int> SelectedLevels { get; set; } = [];
-    public List<string> Categories
-    {
-        get => SelectedRoutes;
-        set => SelectedRoutes = value ?? new List<string>();
-    }
-    public List<LearningType> LearningTypes
-    {
-        get => SelectedTrainingTypes;
-        set => SelectedTrainingTypes = value ?? new List<LearningType>();
-    }
-    public List<int> LevelCodes
-    {
-        get => SelectedLevels;
-        set => SelectedLevels = value ?? new List<int>();
-    }
     public int Total { get; set; }
 
     public int TotalFiltered { get; set; }
@@ -124,15 +109,15 @@ public class CoursesViewModel : PageLinksViewModelBase
                 [
                     CreateCheckboxListFilterSection(
                         "types-filter",
-                        nameof(LearningTypes),
+                        nameof(FilterType.LearningTypes),
                         TrainingTypesSectionHeading,
                         null,
                         LearningTypesFilterHelper.BuildItems(SelectedTrainingTypes),
                         TrainingTypeFindOutMoreText,
                         TrainingTypeFindOutMoreLink
                     ),
-                    CreateCheckboxListFilterSection("levels-filter", nameof(LevelCodes), LevelsSectionHeading, null, GenerateLevelFilterItems(), LevelInformationDisplayText, LevelInformationUrl),
-                    CreateCheckboxListFilterSection("categories-filter", nameof(Categories), CategoriesSectionHeading, null, GenerateRouteFilterItems())
+                    CreateCheckboxListFilterSection("levels-filter", nameof(Levels), LevelsSectionHeading, null, GenerateLevelFilterItems(), LevelInformationDisplayText, LevelInformationUrl),
+                    CreateCheckboxListFilterSection("categories-filter", nameof(FilterType.Categories), CategoriesSectionHeading, null, GenerateRouteFilterItems())
                 ]
             )
             ],
